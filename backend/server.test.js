8ef60515504505293fb39187991a121c8c7a6b41
Manjeet Singh <149746052+manjeetsingh78@users.noreply.github.com>
2025-12-08 17:18:56 +0530

    Update server.test.js

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
-const app = require("./app");
+const app = require("./server");
 
 test("server responds", async () => {
   const res = await request(app).get("/");
-  expect(res.statusCode).toBe(200);
+  expect(res.statusCode).toBe(404); // Root route doesn't exist = 404
 });

b2db3b9a990e009571cb100dfd9e2d0b7c203098
Manjeet Singh <149746052+manjeetsingh78@users.noreply.github.com>
2025-12-21 19:26:28 +0530

    Refactor server tests to use describe block

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,7 +1,9 @@
 const request = require("supertest");
 const app = require("./server");
 
-test("server responds", async () => {
-  const res = await request(app).get("/");
-  expect(res.statusCode).toBe(404); // Root route doesn't exist = 404
+describe("Server basic tests", () => {
+  test("unknown route returns 404", async () => {
+    const res = await request(app).get("/random");
+    expect(res.statusCode).toBe(404);
+  });
 });

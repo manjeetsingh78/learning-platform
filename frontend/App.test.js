96c9e549a9aee4ab122a791c3d66cea488edd989
Manjeet Singh <149746052+manjeetsingh78@users.noreply.github.com>
2025-12-21 19:27:15 +0530

    Rename test to 'renders LearnHub app'

--- frontend/App.test.js
+++ frontend/App.test.js
@@ -1,6 +1,7 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders app", () => {
+test("renders LearnHub app", () => {
   render(<App />);
+  expect(screen.getByText(/LearnHub/i)).toBeInTheDocument();
 });
